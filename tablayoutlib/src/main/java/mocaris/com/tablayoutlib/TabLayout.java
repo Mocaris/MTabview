50863084b62ccb9b63e0f13f5666d6d87548aa03
Mocaris <zp1010@vip.qq.com>
2018-04-12 14:54:47 +0800

    待完善

---
+++ tablayoutlib/src/main/java/mocaris/com/tablayoutlib/TabLayout.java
@@ -0,0 +1,24 @@
+//package mocaris.com.tablayoutlib;
+//
+//import android.content.Context;
+//import android.support.annotation.Nullable;
+//import android.util.AttributeSet;
+//import android.widget.LinearLayout;
+//
+//public class TabLayout extends LinearLayout {
+//    public TabLayout(Context context) {
+//        this(context, null);
+//    }
+//
+//    public TabLayout(Context context, @Nullable AttributeSet attrs) {
+//        super(context, attrs);
+//    }
+//
+//
+//    @Override
+//    protected void onFinishInflate() {
+//        super.onFinishInflate();
+//        int childCount = getChildCount();
+//
+//    }
+//}
